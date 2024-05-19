f508bc7c42cdbcf88c24d3546606e832a0cb9201
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-05-19 19:41:19 +0300

    new tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 """testing storage"""
 import unittest
+import json
 from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models.engine.file_storage import FileStorage
 
 
@@ -10,19 +18,32 @@ class StorageTest(unittest.TestCase):
     def setUp(self):
         """setting"""
         self.inst = FileStorage()
-        self.objs = FileStorage.__objects
 
     def test_all(self):
         """teat all"""
         dic = self.inst.all()
-        self.assertEqual(dic, self.objs)
-        self.assertEqual(type(self.inst.all()), type({}))
+        self.assertEqual(type(dic), dict)
+
+    def test_new(self):
+        """test new mth"""
+        old = len(self.inst.all())
+        new_inst = BaseModel()
+        storage.new(new_inst)
+        new = len(self.inst.all())
+        self.assertNotEqual(old, new)
 
     def test_save(self):
         """test saving method"""
-        with open(self.inst.__file_path, 'r') as f:
+        with open('file.json', 'r') as f:
             a = f.read()
         self.assertEqual(type(a), type(""))
+        with self.assertRaises(TypeError):
+            storage.save(None)
+
+    def test_reload(self):
+        """test reload"""
+        with self.assertRaises(TypeError):
+            storage.reload(None)
 
 
 if __name__ == '__main__':
