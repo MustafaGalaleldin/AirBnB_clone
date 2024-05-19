136c12a0cd9da30c839da52922c7d904504c4ad8
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-05-19 23:04:59 +0300

    tests added

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+""" testing """
+import unittest
+from models.review import Review
+
+
+class Base_test(unittest.TestCase):
+    """ testing base model """
+    def setUp(self):
+        """set"""
+        self.inst = Review()
+
+    def test_str(self):
+        """return string"""
+        self.assertEqual(type(self.inst.__str__()), type(""))
+
+    def test_to_dict(self):
+        """test saving method"""
+        self.assertEqual(type(self.inst.to_dict()), type({}))
+
+
+if __name__ == '__main__':
+    unittest.main()
